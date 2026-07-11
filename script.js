18053c95c56cbaece47aaf571c3939e893a15287
hiraduncut-beep <hiraduncut@gmail.com>
2026-07-12 02:32:56 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+// VØID Particle Stars
+
+const background = document.querySelector(".background");
+
+for(let i = 0; i < 120; i++){
+
+    let star = document.createElement("span");
+
+    star.className = "star";
+
+    star.style.left = Math.random() * 100 + "%";
+    star.style.top = Math.random() * 100 + "%";
+
+    star.style.animationDuration =
+    (Math.random() * 5 + 3) + "s";
+
+    background.appendChild(star);
+
+}
+
+
+// Mouse glow effect
+
+const glow = document.createElement("div");
+
+glow.className = "glow";
+
+document.body.appendChild(glow);
+
+
+document.addEventListener("mousemove", e=>{
+
+    glow.style.left = e.clientX + "px";
+    glow.style.top = e.clientY + "px";
+
+});
+const audio = document.getElementById("audio");
+const play = document.getElementById("play");
+
+const songs = [
+    "assets/music/song1.mp3",
+    "assets/music/song2.mp3",
+    "assets/music/song3.mp3"
+];
+
+let playing = false;
+
+
+if(play){
+
+play.addEventListener("click",()=>{
+
+    if(audio.paused){
+
+        audio.play();
+
+        play.innerHTML="❚❚";
+
+    }else{
+
+        audio.pause();
+
+        play.innerHTML="▶";
+
+    }
+
+});
+
+}
+
+
+function playSong(number){
+
+    audio.src = songs[number];
+
+    audio.play();
+
+    play.innerHTML="❚❚";
+
+}
